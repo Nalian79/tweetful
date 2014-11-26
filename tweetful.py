c5ff536c34f05647cd9e5a19d8c4f483023d5ecb
Michele <michele.morelock@gmail.com>
2014-11-26 21:03:54 +0000

    Renamed commands, added get_user_timeline function.
    First attempt to get another user's timeline data - so far auth fails for me.

--- tweetful.py
+++ tweetful.py
@@ -19,39 +19,54 @@ def make_parser():
     subparsers = parser.add_subparsers(dest="command", help="Available commands")
 
     # Subparser for the timeline command
-    logging.debug("Creating the timeline subparser")
-    timeline_parser = subparsers.add_parser("timeline",
-                                            help = "Get your timeline")
+    logging.debug("Creating the home timeline subparser")
+    timeline_parser = subparsers.add_parser("home",
+                                            help = "Get your own timeline")
 
     # Subparser for the tweet command
     logging.debug("Creating the tweet subparser")
     tweet_parser = subparsers.add_parser("tweet",
                                          help="Update your twitter status!")
-    tweet_parser.add_argument("update", 
+    tweet_parser.add_argument("update",
                               help="Content of your post - limit 140 chars!")
+
+    # Subparser for getting a user's timeline
+    logging.debug("Creating the get subparser")
+    get_parser = subparsers.add_parser("get",
+                                          help="Get twitter data foruser")
+    get_parser.add_argument("screen_name", help="Name of the twitter user")
+    get_parser.add_argument("update_num", help="Number of posts for user",
+                            nargs="?", default=5)
     return parser
 
-def get_timeline(auth):
+def get_home_timeline(auth):
     """ Get the contents of your timeline """
     get_timeline = requests.get(TIMELINE_URL, auth=auth)
-    timeline = json.dumps(get_timeline.json(), indent=2)
+    timeline = json.dumps(get_timeline.json(), indent=4)
     return timeline
 
 def make_tweet(update, auth):
     """ Post an update to your twitter status """
     post_tweet = requests.post(UPDATE_URL, params="status=" + update, auth=auth)
-    tweet = json.dumps(post_tweet.json(), indent=2)
+    tweet = json.dumps(post_tweet.json(), indent=4)
     return tweet
 
-def pretty_print(dump_file):
+def print_screenname_tweet(dump_file):
     """Print the json formatted contents in a more human readable way
 
     Print out only the screen name and tweet text from the input data.
     """
-    pretty_output = json.loads(dump_file)
-    for item in pretty_output:
+    dict_output = json.loads(dump_file)
+    for item in dict_output:
         print item['user']['screen_name'], item['text']
 
+def get_user_timeline(screen_name, count, auth):
+    """Get the twitter timeline of a specific user """
+    user_url = SCREENNAME_URL
+    user_url = user_url.format(screen_name = screen_name, count=count)
+    get_user_timeline = requests.get(user_url, auth=auth)
+    user_timeline = json.dumps(get_user_timeline.json(), indent=4)
+    return user_timeline
 
 def main():
     """ Main function """
@@ -65,18 +80,21 @@ def main():
     logging.debug("Arguments are now: {!r}".format(arguments))
     command = arguments.pop("command")
 
-#    response = requests.get(TIMELINE_URL, auth=auth)
-#    print json.dumps(response.json(), indent=4)
-
     if command == "tweet":
         update = arguments.pop("update")
         tweet = make_tweet(update, auth)
         print tweet
 
+    elif command == "home":
+        my_timeline = get_home_timeline(auth)
+        print_screenname_tweet(my_timeline)
 
-    elif command == "timeline":
-        timeline = get_timeline(auth)
-        pretty_print(timeline)
+    elif command == "get":
+        screen_name = arguments.pop("screen_name")
+        count = arguments.pop("update_num")
+        user_tl = get_user_timeline(screen_name, count, auth)
+        print user_tl
+#        print_screenname_tweet(user_tl)
 
 if __name__ == "__main__":
     main()
--- urls.py
+++ urls.py
@@ -4,3 +4,5 @@ AUTHORIZE_URL = API_URL + "/oauth/authorize?oauth_token={request_token}"
 ACCESS_TOKEN_URL = API_URL + "/oauth/access_token"
 TIMELINE_URL = API_URL + "/1.1/statuses/home_timeline.json"
 UPDATE_URL = API_URL + "/1.1/statuses/update.json"
+SCREENNAME_URL = API_URL + \
+    "/1.1/statuses/user_timeline.json?={screen_name}=twitterapi&count={count}"
